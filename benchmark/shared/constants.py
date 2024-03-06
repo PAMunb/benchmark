c03261140e0db5e9b6cba2e59c579c760b19d299
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-03-05 22:32:56 -0300

    feat: add fuzzing type other_directed_greybox

--- benchmark/shared/constants.py
+++ benchmark/shared/constants.py
@@ -5,6 +5,7 @@ this modules contains the constants used by other modules of the project
 FUZZING_TYPE_BLACKBOX_FUZZING = "blackbox"
 FUZZING_TYPE_GREYBOX_FUZZING = "greybox"
 FUZZING_TYPE_DIRECTED_GREYBOX_FUZZING = "directed_greybox"
+FUZZING_TYPE_OTHER_DIRECTED_GREYBOX_FUZZING = "other_directed_greybox"
 
 DURATION_1M = "1m"
 DURATION_5M = "5m"
